cd5791fbf3fbc452164c83dcde9d1dbad3672942
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-03-25 22:25:58 +0900

    Refactor: React Icon and CSS Conditional Rendering Logic

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,48 +1,59 @@
 import { NavLink } from "react-router-dom";
+import { GrOverview, GrAnalytics } from "react-icons/gr";
+import { DiGoogleAnalytics } from "react-icons/di";
+import { GoProjectRoadmap } from "react-icons/go";
 
 function Navbar() {
   return (
-    <div className="w-150 bg-gray-600 text-white">
-      <div className="flex flex-col items-start justify-start text-white py-4">
+    <div className="w-150 bg-gray-200">
+      <div className="flex flex-col items-start justify-start">
         <NavLink
           to="/dashboard/overview"
           className={({ isActive }) =>
             isActive
-              ? "py-2 px-4 hover:bg-gray-700 w-full bg-gray-900"
-              : "py-2 px-4 hover:bg-gray-700 w-full"
+              ? "flex items-center py-15 px-4 hover:bg-indigo-500 w-full bg-indigo-600 text-white"
+              : "flex items-center py-15 px-4 hover:bg-indigo-500 w-full"
           }
+          style={{ transition: "background-color 0.5s, color 0.5s" }}
         >
+          <GrOverview className="mr-10" />
           개요
         </NavLink>
         <NavLink
           to="/dashboard/projects"
           className={({ isActive }) =>
             isActive
-              ? "py-2 px-4 hover:bg-gray-700 w-full bg-gray-900"
-              : "py-2 px-4 hover:bg-gray-700 w-full"
+              ? "flex items-center py-15 px-4 hover:bg-indigo-500 w-full bg-indigo-600 text-white"
+              : "flex items-center py-15 px-4 hover:bg-indigo-500 w-full"
           }
+          style={{ transition: "background-color 0.5s, color 0.5s" }}
         >
-          api key 발급받기
+          <GoProjectRoadmap className="mr-10" />
+          프로젝트 생성하기
         </NavLink>
         <NavLink
           to="/dashboard/analytics/behavior"
           className={({ isActive }) =>
             isActive
-              ? "py-2 px-4 hover:bg-gray-700 w-full bg-gray-900"
-              : "py-2 px-4 hover:bg-gray-700 w-full"
+              ? "flex items-center py-15 px-4 hover:bg-indigo-500 w-full bg-indigo-600 text-white"
+              : "flex items-center odd:py-15 px-4 hover:bg-indigo-500 w-full"
           }
+          style={{ transition: "background-color 0.5s, color 0.5s" }}
         >
-          행동 흐름 분석 상세
+          <DiGoogleAnalytics className="mr-10" />
+          행동 흐름 분석
         </NavLink>
         <NavLink
           to="/dashboard/analytics/conversion"
           className={({ isActive }) =>
             isActive
-              ? "py-2 px-4 hover:bg-gray-700 w-full bg-gray-900"
-              : "py-2 px-4 hover:bg-gray-700 w-full"
+              ? "flex items-center py-15 px-4 hover:bg-indigo-500 w-full bg-indigo-600 text-white"
+              : "flex items-center py-15 px-4 hover:bg-indigo-500 w-full"
           }
+          style={{ transition: "background-color 0.5s, color 0.5s" }}
         >
-          전환율 상세
+          <GrAnalytics className="mr-10" />
+          전환율 분석
         </NavLink>
       </div>
     </div>
