d50905acf03b0a8d03beaf469e824c41e6f16574
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-04-15 04:43:02 +0900

    Test: add a test for Header

---
+++ src/spec/src/components/Header.spec.jsx
@@ -0,0 +1,74 @@
+import { describe, it, expect, beforeEach, vi } from "vitest";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { signInWithPopup, signOut } from "firebase/auth";
+import Header from "../../../components/Header/index.jsx";
+import { useUserStore, useHeaderStateStore } from "../../../store/store.js";
+
+vi.mock("../../../store/store.js", () => ({
+  useUserStore: vi.fn(() => ({
+    user: null,
+    setUser: vi.fn(),
+    isLoggedIn: false,
+    setIsLoggedIn: vi.fn(),
+  })),
+  useHeaderStateStore: vi.fn(() => ({
+    headerState: "Home",
+  })),
+}));
+
+vi.mock("axios", async (importOriginal) => {
+  const actual = await importOriginal();
+
+  return {
+    ...actual,
+    get: vi.fn(() =>
+      Promise.resolve({
+        data: { result: true, user: { id: "user1", name: "John Doe" } },
+      }),
+    ),
+    post: vi.fn(),
+    delete: vi.fn(),
+  };
+});
+
+vi.mock("firebase/auth", async (importOriginal) => {
+  const actual = await importOriginal();
+
+  return {
+    ...actual,
+    signInWithPopup: vi.fn(),
+    signOut: vi.fn(),
+  };
+});
+
+describe("Header Component", () => {
+  const mockSetUser = vi.fn();
+  const mockSetIsLoggedIn = vi.fn();
+
+  beforeEach(() => {
+    vi.resetAllMocks();
+    useUserStore.mockReturnValue({
+      user: null,
+      setUser: mockSetUser,
+      isLoggedIn: false,
+      setIsLoggedIn: mockSetIsLoggedIn,
+    });
+    useHeaderStateStore.mockReturnValue({ headerState: "Home" });
+    signInWithPopup.mockResolvedValue({
+      user: { id: "user1", name: "John Doe", photoURL: "url" },
+    });
+    signOut.mockResolvedValue();
+  });
+
+  it("renders correctly", () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByText("FlowCatcher")).toBeInTheDocument();
+    expect(screen.getByText("Sign In")).toBeInTheDocument();
+  });
+});
