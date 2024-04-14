94249c7102ed5c067a51e141d61f86a00f467d0f
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-04-15 04:43:34 +0900

    Test: add a test for Navbar

---
+++ src/spec/src/components/Navbar.spec.jsx
@@ -0,0 +1,56 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+
+import Navbar from "../../../components/NavBar";
+
+vi.mock("react-router-dom", async (importOriginal) => {
+  const actual = await importOriginal();
+
+  return {
+    ...actual,
+    NavLink: vi.fn(({ children, to, className }) => (
+      <div
+        data-testid={`NavLink-${to}`}
+        className={className({ isActive: to === "/dashboard/overview" })}
+      >
+        {children}
+      </div>
+    )),
+  };
+});
+
+describe("Navbar Component", () => {
+  it("renders correctly with active and inactive links", () => {
+    render(
+      <MemoryRouter initialEntries={["/dashboard/overview"]}>
+        <Navbar />
+      </MemoryRouter>,
+    );
+
+    expect(
+      screen.getByTestId("NavLink-/dashboard/overview").className,
+    ).toContain("bg-indigo-600");
+    expect(
+      screen.getByTestId("NavLink-/dashboard/overview").className,
+    ).toContain("text-white");
+    expect(
+      screen.getByTestId("NavLink-/dashboard/projects").className,
+    ).not.toContain("bg-indigo-600");
+    expect(
+      screen.getByTestId("NavLink-/dashboard/projects").className,
+    ).not.toContain("text-white");
+    expect(
+      screen.getByTestId("NavLink-/dashboard/analytics/behavior").className,
+    ).not.toContain("bg-indigo-600");
+    expect(
+      screen.getByTestId("NavLink-/dashboard/analytics/behavior").className,
+    ).not.toContain("text-white");
+    expect(
+      screen.getByTestId("NavLink-/dashboard/analytics/conversion").className,
+    ).not.toContain("bg-indigo-600");
+    expect(
+      screen.getByTestId("NavLink-/dashboard/analytics/conversion").className,
+    ).not.toContain("text-white");
+  });
+});
