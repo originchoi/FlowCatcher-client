3d5b786bae3a57dc0820e51adc840b3e8937905b
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-04-15 11:48:32 +0900

    Fix: delete return home related div

--- src/components/MobileAccess/index.jsx
+++ src/components/MobileAccess/index.jsx
@@ -11,14 +11,6 @@ function MobileAcess() {
           Please visit us on a desktop browser. Thank you for your
           understanding!
         </p>
-        <div className="mt-4">
-          <a
-            href="/"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Return Home
-          </a>
-        </div>
       </div>
     </div>
   );
