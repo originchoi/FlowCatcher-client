556b91a062eda4faf84d07998e60b184bf82c00d
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-05-23 02:40:13 +0900

    Refactor: detach the processPageViewData function and add the file

---
+++ src/utils/processPageViewData.js
@@ -0,0 +1,76 @@
+function processPageViewData(sessions) {
+  const nodes = [];
+  const links = [];
+  const linkCounts = {};
+  const visitCounts = {};
+  const exitCounts = {};
+
+  sessions.forEach((session) => {
+    session.pageViews.forEach((pageView, index) => {
+      if (!visitCounts[pageView.url]) {
+        visitCounts[pageView.url] = 1;
+      } else {
+        visitCounts[pageView.url] += 1;
+      }
+
+      let node = nodes.find((n) => n.id === pageView.url);
+
+      if (!node) {
+        node = {
+          id: pageView.url,
+          pageTitle: pageView.pageTitle,
+          referrer: pageView.referrer,
+          timestamp: pageView.timestamp,
+          visitCounts: visitCounts[pageView.url],
+          exitCounts: 0,
+        };
+
+        nodes.push(node);
+      }
+
+      if (!session.isActive && index === session.pageViews.length - 1) {
+        if (!exitCounts[pageView.url]) {
+          exitCounts[pageView.url] = 1;
+        } else {
+          exitCounts[pageView.url] += 1;
+        }
+
+        node.exitCounts = exitCounts[pageView.url];
+      }
+
+      if (index < session.pageViews.length - 1) {
+        const source = pageView.url;
+        const target = session.pageViews[index + 1].url;
+        const linkKey = `${source}-${target}`;
+
+        if (!linkCounts[linkKey]) {
+          linkCounts[linkKey] = 1;
+          links.push({
+            source,
+            target,
+            isSelfLoop: source === target,
+            count: 1,
+          });
+        } else {
+          linkCounts[linkKey] += 1;
+
+          const linkIndex = links.findIndex(
+            (link) => link.source === source && link.target === target,
+          );
+
+          if (linkIndex !== -1) {
+            links[linkIndex].count = linkCounts[linkKey];
+          }
+        }
+      }
+    });
+  });
+
+  nodes.forEach((node) => {
+    node.visitCount = visitCounts[node.id];
+  });
+
+  return { nodes, links };
+}
+
+export default processPageViewData;
