06c3569a9a3252ed893f3324dd093756cb9ad82f
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-05-30 16:36:54 +0900

    Refactor: change About Us and Contact links to anchor tags

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,21 +5,21 @@ function Footer() {
     <footer className="w-full bg-gray-300 text-center px-4 py-20 mt-8 mx-auto z-10">
       <div className="max-w-screen-xl mx-auto px-4">
         <div className="mt-4">
-          <Link to="/about" className="text-blue-600 hover:underline mr-20">
+          <a
+            href="https://github.com/originchoi/FlowCatcher-client"
+            className="text-blue-600 hover:underline mr-20"
+          >
             About Us
-          </Link>
-          <Link to="/contact" className="text-blue-600 hover:underline mr-20">
+          </a>
+          <a
+            href="mailto:kyun4525@gmail.com"
+            className="text-blue-600 hover:underline mr-20"
+          >
             Contact
-          </Link>
+          </a>
           <Link to="/policy" className="text-blue-600 hover:underline mr-20">
             Privacy Policy
           </Link>
-          <a
-            href="https://github.com/originchoi"
-            className="text-blue-600 hover:underline"
-          >
-            Github
-          </a>
         </div>
         <div className="text-sm text-gray-700 mt-10">
           © {new Date().getFullYear()} FlowCatcher. All rights reserved.
