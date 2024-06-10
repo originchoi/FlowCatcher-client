4d98509a19bfab8d8f0a34a1107855e8f12d80a5
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-06-10 14:37:48 +0900

    Style: remove annotation

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -5,7 +5,7 @@ interface ImportMetaEnv {
   readonly VITE_AUTH_DOMAIN: string;
   readonly VITE_PROJECT_ID: string;
   readonly VITE_STORAGE_BUCKET: string;
-  readonly VITE_MESSAGING_SENDER_ID: string; // 여기서는 string으로 처리합니다.
+  readonly VITE_MESSAGING_SENDER_ID: string;
   readonly VITE_APP_ID: string;
   readonly VITE_MEASUREMENT_ID: string;
   readonly VITE_SERVER_URL: string;
