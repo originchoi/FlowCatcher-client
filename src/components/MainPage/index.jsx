af7412a1ec9f24f076b8e7df8595aeb703f69ed7
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-06-24 04:56:44 +0900

    Feat: add a description modal when clicking on a card

--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import Header from "../Header";
 import Footer from "../Footer";
+import Modal from "../Shared/Modal";
 
 import { useHeaderStateStore } from "../../store/store";
 import handleScrollToContent from "../../utils/handleScrollToContent";
@@ -10,12 +11,36 @@ import useGetStartedClick from "../../hooks/useGetStartedClick";
 function MainPage() {
   const { setHeaderState } = useHeaderStateStore();
   const handleGetStartedClick = useGetStartedClick();
+
   const contentRef = useRef(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedCard, setSelectedCard] = useState(null);
+
+  const cards = [
+    {
+      title: "User Flow Analytics",
+      description: "Track User Interactions 🚀",
+      image: "/assets/user_flow.png",
+      gifs: [
+        "/assets/servicecode_issued.gif",
+        "/assets/servicecode_apply.gif",
+        "/assets/behavior_analytics.gif",
+      ],
+    },
+    {
+      title: "Conversion Rate Analytics",
+      description: "Optimize User Flow 🔍",
+      image: "/assets/conversion_analytics.png",
+      gifs: [],
+      customMessage: "해당 기능 업데이트 중입니다. 🙇🏻‍♂️",
+    },
+  ];
 
   useEffect(() => {
     setHeaderState("MainPage");
 
     const container = document.querySelectorAll(".card-container");
+
     container.forEach((card) => {
       card.style.willChange = "transform";
 
@@ -48,6 +73,11 @@ function MainPage() {
       });
     });
   }, []);
+
+  function handleCardClick(card) {
+    setSelectedCard(card);
+    setIsModalOpen(true);
+  }
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -108,34 +138,112 @@ function MainPage() {
             Get Started
           </button>
           <div className="w-full p-4 flex flex-wrap justify-center items-center gap-30">
-            <div className="card-container bg-white rounded-lg shadow-lg p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 relative">
-              <div className="absolute w-full h-full bg-gradient-to-r from-transparent  transition-all"></div>
-              <img
-                src="/assets/user_flow.png"
-                alt="Description"
-                className="w-full h-150 object-contain rounded-t-lg"
-              />
-              <div className="border-t border-gray-300 mt-4 pt-4">
-                <h3 className="font-bold text-lg">User Flow Analytics</h3>
-                <p>Track User Interactions 🚀</p>
+            {cards.map((card, index) => (
+              <div
+                key={index}
+                className="card-container bg-white rounded-lg shadow-lg p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 relative cursor-pointer"
+                onClick={() => handleCardClick(card)}
+                role="button"
+                tabIndex={0}
+                onKeyDown={(e) => {
+                  if (e.key === "Enter" || e.key === " ") {
+                    handleCardClick(card);
+                  }
+                }}
+              >
+                <div className="absolute w-full h-full bg-gradient-to-r from-transparent transition-all"></div>
+                <img
+                  src={card.image}
+                  alt="Description"
+                  className="w-full h-150 object-contain rounded-t-lg"
+                />
+                <div className="border-t border-gray-300 mt-4 pt-4">
+                  <h3 className="font-bold text-lg">{card.title}</h3>
+                  <p>{card.description}</p>
+                </div>
               </div>
-            </div>
-            <div className="card-container bg-white rounded-lg shadow-lg p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 mb-4 relative">
-              <div className="absolute w-full h-full bg-gradient-to-r from-transparent transition-all"></div>
-              <img
-                src="/assets/conversion_analysis.png"
-                alt="Description"
-                className="w-full h-150 object-contain rounded-t-lg"
-              />
-              <div className="border-t border-gray-300 mt-4 pt-4">
-                <h3 className="font-bold text-lg">Conversion Rate Analytics</h3>
-                <p>Optimize User Flow 🔍</p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
       <Footer />
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        width="max-w-2xl"
+      >
+        {selectedCard && (
+          <div className="flex flex-col items-center">
+            <h3 className="text-2xl font-bold mt-10 mb-30">
+              How to use {selectedCard.title}
+            </h3>
+            {selectedCard.gifs.length > 0 && (
+              <>
+                <div className="w-full h-auto mb-4 rounded-lg">
+                  <p className="inline-block text-lg text-white bg-blue-500 bg-opacity-80 px-5 py-1 rounded mb-2">
+                    Step 1
+                  </p>
+                  <img
+                    src={selectedCard.gifs[0]}
+                    alt={`${selectedCard.title} gif 1`}
+                    className="w-full h-auto rounded-lg"
+                  />
+                </div>
+                <ul className="list-disc ml-25 mt-10 mb-40 text-left text-gray-600">
+                  <li>
+                    본인의 웹사이트에 사용할 서비스 코드를 발급받기 위해
+                    사용자는 프로젝트를 만들 수 있습니다.
+                  </li>
+                  <li>
+                    프로젝트별로 서비스 코드를 발급 받을 수 있고, copy 버튼으로
+                    복사할 수 있습니다.
+                  </li>
+                </ul>
+                <div className="w-full h-auto mb-4 rounded-lg">
+                  <p className="inline-block text-lg text-white bg-blue-500 bg-opacity-80 px-5 py-1 rounded mb-2">
+                    Step 2
+                  </p>
+                  <img
+                    src={selectedCard.gifs[1]}
+                    alt={`${selectedCard.title} gif 2`}
+                    className="w-full h-auto rounded-lg"
+                  />
+                </div>
+                <ul className="list-disc ml-25 mt-10 mb-40 text-left text-gray-600">
+                  <li>
+                    발급받은 서비스 코드(스크립트 코드)를 사용자의 비즈니스
+                    웹사이트에 붙여넣기 하여 사용합니다.
+                  </li>
+                </ul>
+                <div className="w-full h-auto mb-4 rounded-lg">
+                  <p className="inline-block text-lg text-white bg-blue-500 bg-opacity-80 px-5 py-1 rounded mb-2">
+                    Step 3
+                  </p>
+                  <img
+                    src={selectedCard.gifs[2]}
+                    alt={`${selectedCard.title} gif 2`}
+                    className="w-full h-auto rounded-lg"
+                  />
+                </div>
+                <ul className="list-disc ml-25 mt-15 text-left text-gray-600">
+                  <li>
+                    행동 흐름 분석 탭에서 서비스 코드를 적용한 웹 사이트 내
+                    방문자들의 페이지 이동 흐름을 볼 수 있습니다.
+                  </li>
+                  <li>
+                    가장 많이 방문한 페이지 5개가 메인페이지 주소 이하 url로
+                    나열되어 그래프와 함께 나타냅니다.
+                  </li>
+                  <li>
+                    가장 하단에 방문자들의 페이지 흐름을 시각화 하여 나타냅니다.
+                  </li>
+                </ul>
+              </>
+            )}
+            <p className="text-lg mt-4">{selectedCard.customMessage}</p>
+          </div>
+        )}
+      </Modal>
     </div>
   );
 }
--- src/components/Shared/Modal.jsx
+++ src/components/Shared/Modal.jsx
@@ -1,21 +1,54 @@
-function Modal({ isOpen, onClose, children }) {
+import { useEffect } from "react";
+import { IoCloseOutline } from "react-icons/io5";
+import { useHeaderStateStore } from "../../store/store";
+
+function Modal({ isOpen, onClose, children, width = "max-w-lg" }) {
+  const { headerState } = useHeaderStateStore();
+  const isMainPage = headerState === "MainPage";
+  const isDashboard = headerState === "Dashboard";
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+
+    return () => {
+      document.body.classList.remove("overflow-hidden");
+    };
+  }, [isOpen]);
+
   if (!isOpen) return null;
 
   return (
     <>
       <div className="fixed inset-0 bg-black bg-opacity-50 z-40" />
       <div className="fixed inset-0 z-50 overflow-auto bg-smoke-light flex">
-        <div className="relative p-8 bg-white w-full max-w-lg h-auto m-auto flex-col flex rounded-lg">
-          {children}
-          <hr className="my-4" />
-          <div className="flex justify-end">
+        <div
+          className={`relative p-8 bg-white w-full ${width} max-h-[70vh] overflow-y-auto m-auto flex-col flex rounded-lg`}
+        >
+          {isMainPage && (
             <button
               onClick={onClose}
-              className="px-4 py-2 bg-gray-200 text-black rounded hover:bg-gray-300"
+              className="absolute top-20 right-13 p-3 text-black rounded-full hover:bg-red-100"
+              aria-label="Close modal"
             >
-              Close
+              <IoCloseOutline size={24} />
             </button>
-          </div>
+          )}
+          {children}
+          <hr className="my-4" />
+          {isDashboard && (
+            <div className="flex justify-end">
+              <button
+                onClick={onClose}
+                className="px-4 py-2 bg-gray-200 text-black rounded hover:bg-gray-300"
+              >
+                Close
+              </button>
+            </div>
+          )}
         </div>
       </div>
     </>
