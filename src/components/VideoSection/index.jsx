9b6f91cf4df3a6a0e5fb47683a847c87659d6484
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-06-24 05:45:34 +0900

    Feat: detach and add a VideoSection from the MainPage component

---
+++ src/components/VideoSection/index.jsx
@@ -0,0 +1,38 @@
+function VideoSection({ contentRef, handleScrollToContent }) {
+  return (
+    <div className="relative h-[90vh] text-center">
+      <video
+        autoPlay
+        loop
+        muted
+        className="absolute top-0 left-0 w-full h-full object-cover"
+        style={{ filter: "blur(1px)" }}
+      >
+        <source src="/videos/background.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="relative z-10 flex flex-col justify-center items-center h-full bg-black bg-opacity-40 p-3 rounded">
+        <div className="text-5xl font-bold text-green-400 animate-floatUp mb-15">
+          FlowCatcher
+        </div>
+        <div className="text-2xl font-bold text-white mt-15 mb-15 animate-floatUp">
+          사용자 행동에 대한 귀중한 데이터를 확보해 보세요
+        </div>
+        <div className="absolute bottom-30 left-1/2 transform -translate-x-1/2">
+          <button
+            aria-label="Scroll down"
+            className="bg-white rounded-full shadow-md hover:shadow-lg transition"
+            onClick={() => handleScrollToContent(contentRef)}
+          >
+            <span className="relative pt-[70px]">
+              <span className="absolute top-0 left-1/2 w-24 h-24 -ml-12 border-l-4 border-b-4 border-white transform rotate-[-45deg] animate-sdb box-border" />
+            </span>
+          </button>
+          <div className="mt-60 text-white text-sm">Scroll</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default VideoSection;
