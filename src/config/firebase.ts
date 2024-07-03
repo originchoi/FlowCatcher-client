6102713856b21232a8c2fdecffd6a6634e7e45fd
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-07-03 22:25:14 +0900

    Refactor: migrate firebase config

---
+++ src/config/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY as string,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN as string,
+  projectId: import.meta.env.VITE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET as string,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID as string,
+  appId: import.meta.env.VITE_APP_ID as string,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID as string,
+};
+
+initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+const auth = getAuth();
+
+export { provider, auth };
